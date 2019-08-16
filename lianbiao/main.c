a93668244527384373c761b76d9c47aaa2a14441
jia jia  <jiajia@jiademacbook-air.local>
2019-08-16 22:38:47 +0800

    Initial Commit

---
+++ lianbiao/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lianbiao
+//
+//  Created by jia jia  on 2019/8/16.
+//  Copyright © 2019年 jia jia . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
